13c7447294c3007c6591101b142d5fe94b095a6b
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-09-18 22:36:33 +0600

    completed maximum subarray problem

---
+++ Leetcode/Maximum_Subarray.rb
@@ -0,0 +1,19 @@
+=begin
+PROBLEM DESCRIPTION:(https://leetcode.com/problems/maximum-subarray/submissions/)
+
+Runtime: 40 ms
+Memory Usage: 10 MB
+=end
+def max_sub_array(nums)
+    n = nums.length
+    dp = Array.new(n,0)
+    dp[0] = nums[0]
+    max = dp[0]       
+    for i in 1...n
+        dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0)
+        max = [max, dp[i]].max
+    end
+    return max
+end
+
+print max_sub_array([-2,1,-3,4,-1,2,1,-5,4]);
