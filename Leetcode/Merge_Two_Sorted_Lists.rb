6640fd863c71d013fd67d4e1f090fab694c62708
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-09-19 22:44:52 +0600

    added the function solution

---
+++ Leetcode/Merge_Two_Sorted_Lists.rb
@@ -0,0 +1,26 @@
+=begin
+PROBLEM DESCRIPTION:(https://leetcode.com/problems/merge-two-sorted-lists/submissions/)
+
+Runtime: 36 ms
+Memory Usage: 9.5 MB
+=end
+
+def merge_two_lists(l1, l2)
+    dummy = cur = ListNode.new(0)
+    while l1 and l2
+        if l1.val < l2.val
+            cur.next = l1
+            l1 = l1.next
+        else
+            cur.next = l2
+            l2 = l2.next
+        end
+        cur = cur.next
+    end
+    if l1 == nil
+        cur.next = l2
+    else
+        cur.next = l1
+    end
+    return dummy.next
+end
