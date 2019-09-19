1b0ea04924d5dc3e8935879ab80aa35c68ae1fa0
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-09-19 22:57:56 +0600

    added solution of longest valid parentheses

---
+++ Leetcode/Longest_Valid_Parentheses.rb
@@ -0,0 +1,24 @@
+=begin
+PROBLEM DESCRIPTION:(https://leetcode.com/problems/longest-valid-parentheses/submissions/)
+
+Runtime: 52 ms
+Memory Usage: 9.7 MB
+=end
+
+def longest_valid_parentheses(s)
+    stk = []
+    stk.push(-1)
+    maxL = 0
+    for i in 0..s.size
+        top = stk[-1]
+        if top != -1 and s[i] == ')' and s[stk[-1]] == '('
+            stk.pop
+            maxL = [maxL, (i-stk[-1])].max
+        else
+            stk.push(i)
+        end
+    end
+    return maxL
+end
+input = ")()())"
+puts longest_valid_parentheses(input)
