36760be351bff141ac458d6e8731f208b8e264ed
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-03-02 22:23:42 +0600

    Adds solution the majority element problem

---
+++ Leetcode/majority_element.rb
@@ -0,0 +1,16 @@
+=begin
+PROBLEM DESCRIPTION:(https://leetcode.com/submissions/detail/262260695/)
+
+Runtime: 36 ms
+Memory Usage: 10.7 MB
+=end
+
+def majority_element(nums)
+    counter = Hash.new(0)
+    nums.each do |val|
+        counter[val] += 1
+    end
+    ans = counter.values.max > (nums.length / 2) ? counter.key(counter.values.max) : 0 
+end
+
+puts majority_element([2,2,1,1,1,2,2])
