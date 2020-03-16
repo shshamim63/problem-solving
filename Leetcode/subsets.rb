5e3180e36c0a63cf33aff451b0663559aaf34dfd
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-03-16 22:00:57 +0600

    :pencil: Adds subsets problem solution
    Adds iterative approach to solve the problem

---
+++ Leetcode/subsets.rb
@@ -0,0 +1,20 @@
+=begin
+PROBLEM DESCRIPTION:(https://leetcode.com/problems/subsets/)
+
+Runtime: 32 ms
+Memory Usage: 9.5 MB
+=end
+
+def subsets(nums)
+    result = []
+    result.push([])
+    nums.each do |n|
+        size = result.length
+        for i in 0...size
+            subset = result[i].dup
+            subset.push(n)
+            result.push(subset)
+        end
+    end
+    result
+end
