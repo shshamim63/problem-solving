37945131ab769985f147d1556ba4a1d8e28b7e9d
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-03-20 21:55:17 +0600

    :pencil: Adds unique binary search tress problem solution
    Adds iterative approach to solve the problem

---
+++ Leetcode/unique_binary_search_trees.rb
@@ -0,0 +1,18 @@
+=begin
+PROBLEM DESCRIPTION:(https://leetcode.com/problems/unique-binary-search-trees/)
+
+Runtime: 32 ms
+Memory Usage: 9.2 MB
+=end
+
+def num_trees(n)
+    total = Array.new(n+1,0)
+    total[0] = 1
+    total[1] = 1
+    for i in 2..n
+        for j in 0...i
+            total[i] += (total[j] * total[i-j-1])
+        end
+    end
+    total.pop
+end
