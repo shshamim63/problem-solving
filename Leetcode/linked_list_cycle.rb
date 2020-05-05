9a7d9843d22db133361e350689dad85662ed0a95
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-05-05 23:28:52 +0600

    :art: Added linked list cycle solution
    Added loop based solution

---
+++ Leetcode/linked_list_cycle.rb
@@ -0,0 +1,20 @@
+=begin
+PROBLEM DESCRIPTION:(https://leetcode.com/problems/linked-list-cycle/)
+
+Runtime: 52 ms
+Memory Usage: 16 MB
+=end
+def hasCycle(head)
+    slow = head if head
+    fast = head.next if head
+    while(fast)
+        if fast.next
+            slow = slow.next
+            fast = fast.next.next
+        else
+            fast = nil
+        end 
+        return true if slow == fast
+    end
+    false
+end
